52c7be15025d1d40989ef199168886d1156851cf
catlyer <catlyer4@gmail.com>
2025-05-29 13:19:26 +0800

    added some stuff to reduce the amount of stray pixels, it should bbe more liney esp for thicker lines, no idea for thin ones tho

--- main.py
+++ main.py
@@ -4,23 +4,23 @@ import pyautogui
 import time
 import matplotlib.pyplot as plt
 
-time.sleep(3)
+#time.sleep(3)
 
 model = "canny"
-show = False
+show = True
 image_path = "image.png"
 scale_factor = 1 # Output will be the exact same as the screenshot so keep that in mind!
 offset_x = 150
 offset_y = 150
 
 image = cv2.imread(image_path, cv2.IMREAD_COLOR)
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 1.4)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image = cv2.GaussianBlur(image, (5, 5), 1.4)
 
-def sobel(blurred_image):
+def sobel(image):
     # Sobel operators
-    Gx = cv2.Sobel(blurred_image, cv2.CV_64F, 1, 0, ksize=3)
-    Gy = cv2.Sobel(blurred_image, cv2.CV_64F, 0, 1, ksize=3)
+    Gx = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=3)
+    Gy = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=3)
 
     # Gradient magnitude
     G = np.sqrt(Gx**2 + Gy**2)
@@ -31,15 +31,25 @@ def sobel(blurred_image):
     G = np.uint8(255 * G / np.max(G))
     return G
 
-def canny(blurred_image):
-    edges = cv2.Canny(blurred_image, 100, 200)
+def canny(image):
+    edges = cv2.Canny(image, 100, 200)
     return edges
+
+def postproc(edges):
+    # unintentional side effect: lines are actually visible now and no longer dogshit!
+    kernel1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
+    post_edges = cv2.dilate(edges, kernel1, iterations=1)
+
+    kernel2 = np.ones((2, 2), np.uint8)
+    final_edge = cv2.morphologyEx(post_edges, cv2.MORPH_CLOSE, kernel2)
+    #final_edge = cv2.medianBlur(post_edges, 3.3)
+    return final_edge
 
 def display(image):
     plt.figure(figsize=(10, 5))
     plt.subplot(1, 1, 1)
     plt.imshow(image, cmap='gray')
-    plt.title('Canny Edge Detection')
+    plt.title('Edge Detection')
     plt.axis('off')
     plt.show()
 
@@ -63,16 +73,18 @@ def draw(edges):
 
 def main():
     if model == "canny":
-        edges = canny(blurred_image)
+        edges = canny(image)
         if show == False:
             draw(edges)
         else:
+            edges = postproc(edges)
             display(edges)
     elif model == "sobel":
-        edges = sobel(blurred_image)
+        edges = sobel(image)
         if show == False:
             draw(edges)
         else:
+            edges = postproc(edges)
             display(edges)
     else:
         print("model does not exist, terminating program")
